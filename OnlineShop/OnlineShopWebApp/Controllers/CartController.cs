3f6c6f1d47137829c0f8ce3a634605bf95a5e564
Zoron87 <zoronbet@gmail.com>
2023-10-11 01:25:28 +0400

    Мелкие правки.

--- OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -4,60 +4,57 @@ using OnlineShopWebApp.Models;
 
 namespace OnlineShopWebApp.Controllers
 {
-	public class CartController : Controller
-	{
-		private Cart cart;
+    public class CartController : Controller
+    {
+        private Cart cart;
         private readonly ICartStorage cartStorage;
 
-		public CartController(ICartStorage cartStorage)
-		{
-			this.cartStorage = cartStorage;
-			cart = cartStorage.TryGetById(ShopUser.Id);
-		}
+        public CartController(ICartStorage cartStorage)
+        {
+            this.cartStorage = cartStorage;
+            cart = cartStorage.TryGetById(ShopUser.Id);
+        }
 
-		public ActionResult Index()
-		{
-			return View(cart);
-		}
+        public ActionResult Index()
+        {
+            return View(cart);
+        }
 
-		public ActionResult Add(int productId, int quantity = 1)
-		{
-			if (productId > 0)
-			{
-				cart = cartStorage.AddItem(productId, quantity);
-				return cart != null ? RedirectToAction("Index") : View("Error");
-			}
+        public ActionResult Add(int productId, int quantity = 1)
+        {
+            if (productId > 0)
+            {
+                cart = cartStorage.AddItem(productId, quantity);
+                return cart != null ? RedirectToAction("Index") : View("Error");
+            }
 
-			return View("Error");
-		}
+            return View("Error");
+        }
 
-		public IActionResult Delete(int productId)
-		{
-			if (productId > 0)
-			{
-				if (cart != null)
-					cart = cartStorage.DeleteItem(productId);
-			}
+        public IActionResult Delete(int productId)
+        {
+            if (cart != null && productId > 0)
+                cart = cartStorage.DeleteItem(productId);
 
-			return RedirectToAction("Index");
-		}
+            return RedirectToAction("Index");
+        }
 
-		public IActionResult Clear()
-		{
-			cart.Items.Clear();
-			return RedirectToAction("Index");
-		}
+        public IActionResult Clear()
+        {
+            cart.Items.Clear();
+            return RedirectToAction("Index");
+        }
 
-		public IActionResult IncreaseProductCount(int productId, int quantity = 1)
-		{
-			cart = cartStorage.Increase(productId, quantity);
-			return RedirectToAction("Index");
-		}
+        public IActionResult Increase(int productId, int quantity = 1)
+        {
+            cart = cartStorage.Increase(productId, quantity);
+            return RedirectToAction("Index");
+        }
 
-		public IActionResult ReduceProductCount(int productId, int quantity = 1)
-		{
-			cart = cartStorage.Reduce(productId, quantity);
-			return RedirectToAction("Index");
-		}
+        public IActionResult Reduce(int productId, int quantity = 1)
+        {
+            cart = cartStorage.Reduce(productId, quantity);
+            return RedirectToAction("Index");
+        }
     }
 }
