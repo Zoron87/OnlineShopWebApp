68c909a43e88f59a15011215c47f3a31cb9ef44a
Zoron87 <zoronbet@gmail.com>
2023-10-10 20:34:02 +0400

    мелкие правки

--- OnlineShop/OnlineShopWebApp/Interfaces/ICartStorage.cs
+++ OnlineShop/OnlineShopWebApp/Interfaces/ICartStorage.cs
@@ -5,12 +5,10 @@ namespace OnlineShopWebApp.Interfaces
 {
     public interface ICartStorage
     {
-        Cart cart { get; }
         Cart AddItem(int productId, int quantity);
         Cart DeleteItem(int productId);
         Cart TryGetById(Guid userGuid);
         Cart Increase(int productId, int quantity = 1);
         Cart Reduce(int productId, int quantity = 1);
-
-	}
+    }
 }
