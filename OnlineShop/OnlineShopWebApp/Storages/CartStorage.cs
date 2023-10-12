18592909b6f77992bdb8d9dca2bc5448449552d7
Zoron87 <zoronbet@gmail.com>
2023-10-12 20:26:19 +0400

    Вернул юзинги

--- OnlineShop/OnlineShopWebApp/Storages/CartStorage.cs
+++ OnlineShop/OnlineShopWebApp/Storages/CartStorage.cs
@@ -1,93 +1,99 @@
-﻿namespace OnlineShopWebApp.Storages
+﻿using OnlineShopWebApp.Interfaces;
+using OnlineShopWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineShopWebApp.Storages
 {
-    public class CartStorage : ICartStorage
-    {
-        private List<Cart> carts = new List<Cart>();
-        private readonly IProductStorage productStorage;
+	public class CartStorage : ICartStorage
+	{
+		private List<Cart> carts = new List<Cart>();
+		private readonly IProductStorage productStorage;
 
-        public CartStorage(IProductStorage productStorage)
-        {
-            this.productStorage = productStorage;
-        }
+		public CartStorage(IProductStorage productStorage)
+		{
+			this.productStorage = productStorage;
+		}
 
-        public Cart Get(Guid userGuid)
-        {
-            return TryGetById(userGuid);
-        }
+		public Cart Get(Guid userGuid)
+		{
+			return TryGetById(userGuid);
+		}
 
-        public Cart AddItem(int productId, int quantity = 1)
-        {
-            var product = productStorage.TryGetById(productId);
+		public Cart AddItem(int productId, int quantity = 1)
+		{
+			var product = productStorage.TryGetById(productId);
 
-            if (product == null)
-                throw new Exception("Указанный товар не обнаружен");
+			if (product == null)
+				throw new Exception("Указанный товар не обнаружен");
 
-            var cartPositon = new CartItem(product);
+			var cartPositon = new CartItem(product);
 
-            var cart = Get(ShopUser.Id);
+			var cart = Get(ShopUser.Id);
 
-            if (cart == null)
-            {
-                cart = new Cart(ShopUser.Id, new List<CartItem>() { cartPositon });
-            }
-            else
-            {
-                var checkSameProduct = cart.Items.Any(cartItem => cartItem.Product.Id == product.Id);
+			if (cart == null)
+			{
+				cart = new Cart(ShopUser.Id, new List<CartItem>() { cartPositon });
+			}
+			else
+			{
+				var checkSameProduct = cart.Items.Any(cartItem => cartItem.Product.Id == product.Id);
 
-                if (checkSameProduct)
-                    cart.Items.FirstOrDefault(cartItem => cartItem.Product.Id == product.Id).Quantity += quantity;
-                else
-                    cart.Items.Add(cartPositon);
-            }
+				if (checkSameProduct)
+					cart.Items.FirstOrDefault(cartItem => cartItem.Product.Id == product.Id).Quantity += quantity;
+				else
+					cart.Items.Add(cartPositon);
+			}
 
-            carts.Add(cart);
+			carts.Add(cart);
 
-            return cart;
-        }
+			return cart;
+		}
 
-        public Cart DeleteItem(int productId)
-        {
-            var cart = Get(ShopUser.Id);
+		public Cart DeleteItem(int productId)
+		{
+			var cart = Get(ShopUser.Id);
 
-            var itemForRemove = cart.Items.FirstOrDefault(cartItem => cartItem.Product.Id == productId);
-            cart.Items.Remove(itemForRemove);
+			var itemForRemove = cart.Items.FirstOrDefault(cartItem => cartItem.Product.Id == productId);
+			cart.Items.Remove(itemForRemove);
 
-            return cart;
-        }
+			return cart;
+		}
 
-        public Cart Increase(int productId, int quantity = 1)
-        {
-            var cart = Get(ShopUser.Id);
+		public Cart Increase(int productId, int quantity = 1)
+		{
+			var cart = Get(ShopUser.Id);
 
-            var product = cart.Items.FirstOrDefault(p => p.Product.Id == productId);
+			var product = cart.Items.FirstOrDefault(p => p.Product.Id == productId);
 
-            if (product != null)
-                product.Quantity += quantity;
-            else
-                throw new Exception("Указанный товар не обнаружен");
+			if (product != null)
+				product.Quantity += quantity;
+			else
+				throw new Exception("Указанный товар не обнаружен");
 
-            return cart;
-        }
+			return cart;
+		}
 
-        public Cart Reduce(int productId, int quantity = 1)
-        {
-            var cart = Get(ShopUser.Id);
+		public Cart Reduce(int productId, int quantity = 1)
+		{
+			var cart = Get(ShopUser.Id);
 
-            var product = cart.Items.FirstOrDefault(p => p.Product.Id == productId);
+			var product = cart.Items.FirstOrDefault(p => p.Product.Id == productId);
 
-            if (product == null) return cart;
+			if (product == null) return cart;
 
-            if (product.Quantity - quantity <= 0)
-                DeleteItem(productId);
-            else
-                product.Quantity -= quantity;
+			if (product.Quantity - quantity <= 0)
+				DeleteItem(productId);
+			else
+				product.Quantity -= quantity;
 
-            return cart;
-        }
+			return cart;
+		}
 
-        public Cart TryGetById(Guid userId)
-        {
-            return carts.FirstOrDefault(c => c.UserId == userId);
-        }
-    }
+		public Cart TryGetById(Guid userId)
+		{
+			return carts.FirstOrDefault(c => c.UserId == userId);
+		}
+	}
 }
