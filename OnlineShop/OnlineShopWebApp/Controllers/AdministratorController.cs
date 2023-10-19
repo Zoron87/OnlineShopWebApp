d3b3f3b2b75ae92dd84a2c3df931bdfc6a274874
Zoron87 <zoronbet@gmail.com>
2023-10-19 17:55:29 +0400

    Syryaev_lesson5_4

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdministratorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetOrders()
+        {
+            return View("GetOrders");
+        }
+
+        public ActionResult GetUsers()
+        {
+            return View("GetUsers");
+        }
+
+        public ActionResult GetRoles()
+        {
+            return View("GetRoles");
+        }
+
+        public ActionResult GetProducts()
+        {
+            return View("GetProducts");
+        }
+    }
+}
