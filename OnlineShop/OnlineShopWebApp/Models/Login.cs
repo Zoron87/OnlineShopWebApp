dd065882ea431906a629469a9a7800d46b6577f3
Zoron87 <zoronbet@gmail.com>
2023-10-22 23:55:22 +0400

    Добавление валидации на формах авторизации и регистрации.

--- OnlineShop/OnlineShopWebApp/Models/Login.cs
+++ OnlineShop/OnlineShopWebApp/Models/Login.cs
@@ -1,8 +1,12 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Login
     {
+        [Required(ErrorMessage="Не указана электронная почта пользователя")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Не указано пароль пользователя")]
         public string Password { get; set; }
         public string IsRememberMe { get; set; }
     }
--- OnlineShop/OnlineShopWebApp/Models/Register.cs
+++ OnlineShop/OnlineShopWebApp/Models/Register.cs
@@ -1,9 +1,14 @@
-﻿namespace OnlineShopWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopWebApp.Models
 {
     public class Register
     {
+        [Required(ErrorMessage="Не указана электронная почта пользователя")]
         public string Email { get; set; }
+        [Required(ErrorMessage="Не указан пароль пользователя")]
         public string Password { get; set; }
+        [Required(ErrorMessage="Не указан повторный пароль пользователя")]
         public string ConfirmPassword { get; set; }
         public string IsRememberMe { get; set; }
     }
